306cdb0c22921e3a5aff7a6990f69f779cddd6f8
C1310418 Anderson da Silva de Oliveira <c1310418@interno.bb.com.br>
2020-10-08 23:02:07 -0300

    entidade de produto e SQL script

---
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Produto.java
@@ -0,0 +1,71 @@
+package com.devsuperior.dscatalog.entities;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = { "id" })
+@Entity
+@Table(name = "tb_produto")
+public class Produto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@Getter
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Getter
+	@Setter
+	@Column(name = "NOME")
+	private String nome;
+
+	@Getter
+	@Setter
+	@Column(name = "DESCRICAO", columnDefinition = "TEXT")
+	private String descricao;
+
+	@Getter
+	@Setter
+	@Column(name = "VALOR")
+	private BigDecimal valor;
+
+	@Getter
+	@Setter
+	@Column(name = "URL_IMAGEM")
+	private String urlImagem;
+
+	@Getter
+	@Setter
+	@Column(name = "DATA", columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant data;
+
+	@Getter
+	@ManyToMany
+	@JoinTable(name = "tb_produto_categoria", 
+		joinColumns = @JoinColumn(name = "produto_id"), 
+		inverseJoinColumns = @JoinColumn(name = "categoria_id"))
+	private Set<Categoria> categorias = new HashSet<Categoria>();
+
+}
